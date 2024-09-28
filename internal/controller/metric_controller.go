8c93cd1ff6ecbaeba721d59518a65c0d784926b3
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-09-28 01:18:19 -0300

    create metric files to routes /metrics

---
+++ internal/controller/metric_controller.go
@@ -0,0 +1,46 @@
+package controller
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/L3onix/frete-rapido-test/internal/usecase"
+	"github.com/gin-gonic/gin"
+)
+
+type metricController struct {
+	usecase usecase.MetricUsecase
+}
+
+func NewMetricController(metricUsecase usecase.MetricUsecase) metricController {
+	return metricController{
+		usecase: metricUsecase,
+	}
+}
+
+func (mc *metricController) GetMetrics(ctx *gin.Context) {
+	lastQuotes := ctx.Query("last_quotes")
+	if lastQuotes != "" {
+		if _, err := strconv.Atoi(lastQuotes); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": "last_quotes precisa ser um número!",
+			})
+			return
+		}
+	}
+
+	metrics, err := mc.usecase.GetMetrics(lastQuotes)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	if metrics == nil {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"error": "Banco de dados não possui métricas",
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, metrics)
+	return
+}
---
+++ internal/usecase/metric_usecase.go
@@ -0,0 +1,17 @@
+package usecase
+
+import "github.com/L3onix/frete-rapido-test/internal/model"
+
+type MetricUsecase struct {
+	carrierUseCase CarrierUseCase
+}
+
+func NewMetricUseCase(carrierUseCase CarrierUseCase) MetricUsecase {
+	return MetricUsecase{
+		carrierUseCase: carrierUseCase,
+	}
+}
+
+func (mu *MetricUsecase) GetMetrics(lastQuotes string) (*[]model.Carrier, error) {
+	return mu.carrierUseCase.GetLastCarriers(lastQuotes)
+}
