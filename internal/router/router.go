50b9b9d1d6198bb30af1214d8d2d1af161bf9936
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-09-30 02:22:20 -0300

    refactor to order routes

--- internal/router/router.go
+++ internal/router/router.go
@@ -23,16 +23,15 @@ func SetupRoutes(database *sql.DB, server *gin.Engine) *gin.Engine {
 	server.GET("/carrier", CarrierController.GetCarriers)
 	server.GET("/carrier/:carrierId", CarrierController.GetCarrierById)
 	server.POST("/carrier", CarrierController.CreateCarrier)
-
-	// rotas definidas pelo teste
-	server.GET("/metrics", MetricController.GetMetrics)
-	server.POST("/quote", QuoteController.CreateQuote)
-
 	server.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "pong!",
 		})
 	})
+
+	// rotas definidas pelo teste
+	server.GET("/metrics", MetricController.GetMetrics)
+	server.POST("/quote", QuoteController.CreateQuote)
 
 	return server
 }
